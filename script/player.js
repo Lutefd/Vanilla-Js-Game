41ea93c022fc9d2cc8df227e84c4106161af5f90
Lutefd <lfaddourado@gmail.com>
2022-08-16 18:30:04 -0300

    First commit

---
+++ script/player.js
@@ -0,0 +1,26 @@
+export class Player {
+  constructor(game) {
+    this.game = game;
+    this.width = 100;
+    this.height = 91.3;
+    this.x = 0;
+    this.y = this.game.height - this.height;
+    this.image = document.getElementById('player');
+    this.frameX = 0;
+    this.frameY = 0;
+  }
+  update() {}
+  draw(context) {
+    context.drawImage(
+      this.image,
+      this.frameX * this.width,
+      this.frameY * this.height,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
