56786a249ea196ae017a4bab62002dcbbb8b930e
Lutefd <lfaddourado@gmail.com>
2022-08-16 21:24:17 -0300

    Added horizontal and vertical movement

--- script/player.js
+++ script/player.js
@@ -8,10 +8,28 @@ export class Player {
     this.image = document.getElementById('player');
     this.frameX = 0;
     this.frameY = 0;
+    this.speed = 0;
+    this.maxSpeed = 10;
+    this.vy = 0;
+    this.weight = 1;
   }
   update(input) {
-    if (input.includes('ArrowRight')) this.x++;
-    else if (input.includes('ArrowLeft')) this.x--;
+    //horizontal movement
+    this.x += this.speed;
+    if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
+    else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
+    else this.speed = 0;
+    if (this.x < 0) this.x = 0;
+    if (this.x > this.game.width - this.width)
+      this.x = this.game.width - this.width;
+    //vertical movement
+    if (input.includes('ArrowUp') && this.onGround()) this.vy = -28;
+    this.y += this.vy;
+    if (!this.onGround()) {
+      this.vy += this.weight;
+    } else this.vy = 0;
+    if (this.y > this.gameHeight - this.height)
+      this.y = this.gameHeight - this.height;
   }
   draw(context) {
     context.drawImage(
@@ -25,5 +43,8 @@ export class Player {
       this.width,
       this.height
     );
+  }
+  onGround() {
+    return this.y >= this.game.height - this.height;
   }
 }
