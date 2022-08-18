ec626313d1781e11126f3da96dea6a1e9d886edf
Lutefd <lfaddourado@gmail.com>
2022-08-18 15:31:05 -0300

    Added diving state

--- script/playerStates.js
+++ script/playerStates.js
@@ -80,6 +80,10 @@ export class Jumping extends State {
       this.game.player.setState(states.FALLING, 1);
     } else if (input.includes('Enter')) {
       this.game.player.setState(states.ROLLING, 2);
+    } else if (input.includes('ArrowDown')) {
+      this.game.player.setState(states.DIVING, 0);
+    } else if (input.includes('s')) {
+      this.game.player.setState(states.DIVING, 0);
     }
   }
 }
@@ -95,6 +99,10 @@ export class Falling extends State {
   handleInput(input) {
     if (this.game.player.onGround()) {
       this.game.player.setState(states.RUNNING, 1);
+    } else if (input.includes('ArrowDown')) {
+      this.game.player.setState(states.DIVING, 0);
+    } else if (input.includes('s')) {
+      this.game.player.setState(states.DIVING, 0);
     }
   }
 }
@@ -131,6 +139,31 @@ export class Rolling extends State {
       this.game.player.onGround()
     ) {
       this.game.player.vy -= 27;
+    }
+  }
+}
+export class Diving extends State {
+  constructor(game) {
+    super('DIVING', game);
+  }
+  enter() {
+    this.game.player.frameX = 0;
+    this.game.player.maxFrame = 6;
+    this.game.player.frameY = 6;
+    this.game.player.vy = 15;
+  }
+  handleInput(input) {
+    this.game.particles.unshift(
+      new Fire(
+        this.game,
+        this.game.player.x + this.game.player.width * 0.5,
+        this.game.player.y + this.game.player.height * 0.5
+      )
+    );
+    if (this.game.player.onGround()) {
+      this.game.player.setState(states.RUNNING, 1);
+    } else if (input.includes('Enter') && this.game.player.onGround()) {
+      this.game.player.setState(states.ROLLING, 2);
     }
   }
 }
